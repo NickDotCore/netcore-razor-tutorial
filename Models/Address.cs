8c8dbe0ed3773352c20eb6a10f1680180dc638f1
Nicholas Browning <Nicholas.Browning@rapidloans.com.au>
2021-05-13 16:29:33 +1000

    initial commit

---
+++ Models/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netcore_razor_tutorial.Models
+{
+    public class Address
+    {
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netcore_razor_tutorial.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public string FirstName { get; set; }
+
+        public void OnGet() // what renders the page
+        {
+            if (string.IsNullOrEmpty(FirstName))
+            {
+                FirstName = "User";
+            }
+        }
+
+        // will be run when you post info to the page
+        public void OnPost()
+        {
+            
+        }
+    }
+}
